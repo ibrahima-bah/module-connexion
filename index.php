f89062b832243682c0fe6f7489bb6ba826794eaf
ibrahima-bah <71871112+ibrahima-bah@users.noreply.github.com>
2020-11-23 13:23:18 +0100

    remise index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet"  href="http://fonts.googleapis.com/css?family=Crete+Round">
+		<link rel="stylesheet" type="text/css" href="style1.css">
+		<meta charset="utf-8">
+		<title>Page index</title>
+	</head>
+	<body>
+		<section id="main-image">
+			<div class="wrapper">
+				<h2>Bienvenue sur <br><strong>mon site Web</strong></h2><br>
+				<div align="center">
+				</div>
+				<br>
+				
+					<a href="deconnexion.php" class="premier">Se deconnecter</a> <br><br>
+					<a href="profil.php" class="premier">Mettre a jour mon profil</a>
+				
+			</div>
+		</section>
+	</body>
+</html>
+
